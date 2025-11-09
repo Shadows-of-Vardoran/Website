4d8e9fd786265681646f57bb78558bd7be99bd10
Travis Kehrli <tmktahu@gmail.com>
2025-11-08 21:47:55 -0800

    cmon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,7 +7,7 @@
 </svelte:head>
 
 <div class="h-screen w-screen flex flex-col overflow-hidden">
-  <div class="text-center h-12 bg-background-700"> top nav bar goes her12e </div>
+  <div class="text-center h-12 bg-background-700"> top nav bar goes here </div>
 
   <div class="w-full h-full overflow-hidden">
     <slot />
