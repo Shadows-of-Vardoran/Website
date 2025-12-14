4a3b2b8aefd52faf70f2d865e5ed5ac188fed9e9
Travis Kehrli <tmktahu@gmail.com>
2025-12-14 00:55:00 -0800

    tweaking for github

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  clearScreen: false,
+  plugins: [sveltekit(), tailwindcss()],
 });
