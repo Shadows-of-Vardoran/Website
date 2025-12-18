dc81bcfacb645f1c1edd60348dc7dc9c6e1dffd4
Travis Kehrli <tmktahu@gmail.com>
2025-12-17 19:22:25 -0800

    front page tweaking

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -26,12 +26,12 @@
     <div class="grid grid-cols-[2fr_240px_2fr] max-lg:flex max-lg:flex-col w-full mt-10">
       <div class="flex flex-col fade-background-left py-2 px-3 -ml-20 pl-20 max-lg:border-r-2 max-lg:border-tprimary-0">
         <div class="text-3xl text-tprimary text-right font-cinzel mb-4">Server Features</div>
-        <div class="flex flex-col gap-1 text-lg text-tsecondary text-right">
-          <div>Dark Fantasy RP | Lore-Driven Worldbuilding -</div>
-          <div>Fully RP-Enforced Environment -</div>
-          <div>Handpicked Mods for Immersion & Balance -</div>
-          <div>PvP-Enabled with Political Intrigue -</div>
-          <div>Community Events, Storylines, and Seasonal Play -</div>
+        <div class="flex flex-col items-end gap-1 text-lg text-tsecondary text-right">
+          <div class="border-b border-red-900 w-fit">Dark Fantasy RP | Lore-Driven Worldbuilding</div>
+          <div class="border-b border-red-900 w-fit">Fully RP-Enforced Environment</div>
+          <div class="border-b border-red-900 w-fit">Handpicked Mods for Immersion & Balance</div>
+          <div class="border-b border-red-900 w-fit">PvP-Enabled with Political Intrigue</div>
+          <div class="border-b border-red-900 w-fit">Community Events, Storylines, and Seasonal Play</div>
         </div>
       </div>
 
@@ -45,19 +45,20 @@
 
       <div class="flex flex-col justify-center fade-background-right py-2 px-3 -mr-20 pr-20 max-lg:border-l-2 max-lg:border-tprimary-0 max-lg:mt-6">
         <div class="text-3xl text-tprimary font-cinzel mb-4">Server Settings</div>
-        <div class="flex gap-10 w-full">
-          <div class="flex flex-col gap-1 text-lg text-tsecondary">
-            <div>- Brutal-Light Mode</div>
-            <div>- 450 Max Castle Tiles</div>
-            <div>- 5 Castle Floors</div>
-            <div>- 20m Days/40m Nights</div>
-            <div>- Unique Soul Shards</div>
+        <div class="flex gap-8 w-full max-xl:flex-col max-xl:gap-2">
+          <div class="flex flex-col gap-1 text-lg text-tsecondary whitespace-nowrap">
+            <div class="border-b border-red-900 w-fit">Brutal-Light Mode</div>
+            <div class="border-b border-red-900 w-fit">450 Max Castle Tiles</div>
+            <div class="border-b border-red-900 w-fit">5 Castle Floors</div>
+            <div class="border-b border-red-900 w-fit">20m Days/40m Nights</div>
+            <div class="border-b border-red-900 w-fit">Unique Soul Shards</div>
           </div>
           <div class="flex flex-col gap-1 text-lg text-tsecondary">
-            <div>- 4 Spells while Unarmed</div>
-            <div>- PVP Enabled</div>
-            <div>- Clan/Sigil Size of 12</div>
-            <div>- 2 Home, 1 Business Heart per Clan/Sigil</div>
+            <div class="border-b border-red-900 w-fit">4 Spells while Unarmed</div>
+            <div class="border-b border-red-900 w-fit">PVP Enabled</div>
+            <div class="border-b border-red-900 w-fit">Clan/Sigil Size of 12</div>
+            <div class="border-b border-red-900 w-fit">3 Hearts per Clan/Sigil...</div>
+            <div class="border-b border-red-900 w-fit">2 Home, 1 Business Heart</div>
           </div>
         </div>
       </div>
